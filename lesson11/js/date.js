919d526e2625de4b1d89671fad7a34620d8aff49
iodaniel <iodaniel@icloud.com>
2020-12-03 11:06:58 -0700

    git commit

--- lesson11/js/date.js
+++ lesson11/js/date.js
@@ -1,4 +1,4 @@
     var getDate = new Date();
-    document.getElementById('demo').innerHTML= "Update is "+ getDate.getDate()+ "/"+getDate.getMonth()+"/" +getDate.getFullYear();
+    document.getElementById('demo').innerHTML= " | Your last Update is "+ String(getDate.getMonth()+1)+"/"+ getDate.getDate()+ "/"+ getDate.getFullYear();
 
   
