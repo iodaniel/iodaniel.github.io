6fa901e4f426251a01684bf58e5413d924f39294
iodaniel <iodaniel@icloud.com>
2020-12-13 17:48:07 -0700

    git commit

---
+++ scoots/js/date.js
@@ -0,0 +1,4 @@
+var getDate = new Date();
+    document.getElementById('demo').innerHTML= " | Your last Update is "+ String(getDate.getMonth()+1)+"/"+ getDate.getDate()+ "/"+ getDate.getFullYear();
+
+  
